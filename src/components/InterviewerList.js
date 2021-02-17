f25efa7ec9f1ed28b16aa7da032d46528f1f09fe
Idiljei <i.jeilani@gmail.com>
2021-02-16 22:05:07 -0700

    Almost doneeee

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,16 +7,16 @@ import "components/InterviewerList.scss";
 export default function InterviewerList(props){
 
   const mappedList = props.interviewers.map(interviewer => {
-    return(
-      
+    
+    return (
       <InterviewerListItem
-        key = {interviewer.id}
-        name={interviewer.name} 
-        avatar={interviewer.avatar} 
-        selected = {interviewer.id === props.interviewer}
-        setInterviewer={(event) => props.setInterviewer(interviewer.id)}
-        />  
-    )
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={event => props.setInterviewer(interviewer.id)}
+      />
+    );
   });
  
 
