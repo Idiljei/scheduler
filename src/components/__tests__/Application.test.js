44e3a2bb38f3ee7881c641014bfa7bc63de0169b
Idiljei <i.jeilani@gmail.com>
2021-02-23 16:42:59 -0700

    Application testing complete

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -98,9 +98,35 @@ describe("Application", () => {
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 
-  // it("shows the save error when failing to save an appointment", () => {
-  //   axios.put.mockRejectedValueOnce();
-  // });
+//   EDITS 
+
+it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+  const { container, debug } = render(<Application />);
+
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  const appointment = getAllByTestId(
+    container,
+    "appointment"
+  ).find((appointment) => queryByText(appointment, "Archie Cohen"));
+
+  fireEvent.click(getByAltText(appointment, "Edit"));
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Idil Jei" },
+  });
+
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  fireEvent.click(getByText(appointment, "Save"));
+
+  expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+  await waitForElement(() => getByText(appointment, "Idil Jei"));
+  const day = getAllByTestId(container, "day").find((day) =>
+    queryByText(day, "Monday")
+  );
+  expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+});
+
 
 // ERROR HANDLING 
 
